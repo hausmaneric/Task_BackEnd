af9a3673f02a7e47e7d2f1e6b5ac52ff1ca221dc
Eric Hausman <109166965+hausmaneric@users.noreply.github.com>
2024-11-27 14:48:26 -0300

    Three Commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template
 from flask_cors import CORS, cross_origin
 from Models.content import *
@@ -117,4 +118,5 @@ def _verificationCode():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))  
+    app.run(host="0.0.0.0", port=port)
